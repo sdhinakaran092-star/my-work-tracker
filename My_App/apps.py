b8f4f018ea372a26807b0cd6c0d33784d52c9812
Dhinakaran.SAGAYANATHAN <Dhinakaran.SAGAYANATHAN@talentproindia.com>
2026-02-27 11:01:12 +0530

    Create superuser for Render

--- My_App/apps.py
+++ My_App/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
 
-
 class MyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'My_App'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        try:
+            if not User.objects.filter(username="dhina").exists():
+                User.objects.create_superuser(
+                    "dhina",              # your username
+                    "dhinakaran092@gmail.com",    # your email
+                    "Dhina@9585"          # your password
+                )
+        except:
+            pass
