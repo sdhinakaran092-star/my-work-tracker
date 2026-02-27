a153a3585932b17276a12b2c3c627c92ebe49cd2
Dhinakaran.SAGAYANATHAN <Dhinakaran.SAGAYANATHAN@talentproindia.com>
2026-02-27 11:49:18 +0530

    final postgres config

--- New_Web/settings.py
+++ New_Web/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'New_Web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
